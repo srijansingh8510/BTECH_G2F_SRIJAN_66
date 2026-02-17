82b3f37b7660e7d1f6cd298777c9c3732f6f61b3
srijansingh8510 <srijansingh8510@gmail.com>
2026-02-17 11:36:51 +0530

    Add files via upload

---
+++ perator_operand.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main ()
+{
+    int choice,a,b ;
+    printf("Select your choice :\n");
+    printf("1- Add :\n");
+    printf("2- Sub :\n");
+    printf("3- Mul :\n");
+    printf("4- Mod :\n");  
+    printf("5- Div :\n");
+       printf("Enter number a= ");
+       scanf("%d",&a);
+       printf("Enter number b= ");
+       scanf("%d",&b);
+       printf("Enter your choice=");
+       scanf("%d",&choice);
+       switch(choice)
+{
+    case 1 :
+    printf("Ädd of a and b%d",(a+b));
+    break;
+    case 2 :
+    printf("Sub of a and b%d",(a-b));
+    break;
+    case 3 :
+    printf("Mul of a and b%d",(a*b));
+    break;
+    case 4 :
+    printf("Div of a and b%d",(a/b));
+    break;
+    case 5 :
+    printf("Mod of a and b%d",(a%b));
+    break;
+    default :
+    printf ("Wrong Choice");
+}
+}
