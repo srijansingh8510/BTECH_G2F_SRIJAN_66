59047ae2b9bff9ae1950ede906de66382fda0fd2
srijansingh8510 <srijansingh8510@gmail.com>
2026-03-24 11:01:55 +0530

    Add files via upload

--- call_by_value.c
+++ call_by_value.c
@@ -1,19 +1,19 @@
+
 #include <stdio.h>
-int factorial(int n)
-{
-    int i, fact = 1;
-    for(i = 1; i <= n; i++)
-    {
-    fact = fact * i;
-    }
-    return fact;
+
+void swap(int x, int y) {
+    int temp = x;
+    x = y;
+    y = temp;
+    printf("Inside function: x=%d, y=%d\n", x, y);
 }
-int main()
-{
-    int num, result;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    result = factorial(num); 
-    printf("Factorial of %d is %d\n", num, result);
+
+int main() {
+    int a = 10, b = 20;
+    
+    printf("Before swap: a=%d, b=%d\n", a, b);
+    swap(a, b);  
+    printf("After swap: a=%d, b=%d\n", a, b);
+    
     return 0;
 }
